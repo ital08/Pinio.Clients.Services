41f4b691f7e4a06f1f6493b8930f1cde8ac658f2
Italo <italo.rivadeneyra.r@uni.pe>
2021-09-27 21:18:49 -0500

    Subida de Archivos a Pinio.Clients

---
+++ src/main/java/com/pinio/services/Application.java
@@ -0,0 +1,14 @@
+package com.pinio.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Pinio.Clients.Services");
+        SpringApplication.run(Application.class, args);
+    }
+
+}
