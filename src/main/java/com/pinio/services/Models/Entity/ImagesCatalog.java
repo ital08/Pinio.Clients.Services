667af45c5fb0501038272a2cfa5dc914b17248e8
Italo <italo.rivadeneyra.r@uni.pe>
2021-10-16 19:40:02 -0500

    version pom

---
+++ src/main/java/com/pinio/services/Models/Entity/ImagesCatalog.java
@@ -0,0 +1,14 @@
+package com.pinio.services.Models.Entity;
+
+public class ImagesCatalog {
+//    CREATE TABLE public.imagescatalog (
+//            idimagescatalog varchar(9) NOT NULL,
+//    idproductcatalog varchar(10) NOT NULL,
+//    urlimage varchar(150) NOT NULL,
+//    position integer NOT NULL,
+//    PRIMARY KEY (idimagescatalog)
+//);
+    private Integer idImagesCatalog;
+    private String idProductCatalog;
+    private String url;
+}
